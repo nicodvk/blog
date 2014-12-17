5986c4d21aca6be0e8d5f6005be7ec930de60254
Nicolas Devynck <nicolas.devynck@epsi.fr>
2014-12-17 10:35:38 +0100

    Add rails admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user && user.type == 'Users::Admin'
+    if user && user.type== 'Users::Admin'
+      can :access, :rails_admin
+      can :dashboard
       can :manage, :all
     elsif user && user.type == 'Users::Writer'
+      can :access, :rails_admin
+      can :dashboard
       can :read, :all
       can [:create, :read, :update, :destroy, :comment], Post, user_id: user.id
-      can [:create, :read, :update, :destroy], Tag, user_id: user.id
+      can [:create, :read, :update, :destroy], Tag
       can [:read, :update], User, id: user.id
     elsif user && user.type == 'Users::Guest'
       can :read, :all
